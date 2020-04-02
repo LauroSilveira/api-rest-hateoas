366734c4a718c01166e13733ec87f0f4e9c20acd
Lauro Silveira <lauro.silveira@ymail.com>
2020-04-02 23:51:47 +0200

    Product model created

---
+++ api/src/main/java/com/example/api/rest/api/model/Product.java
@@ -0,0 +1,62 @@
+package com.example.api.rest.api.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String description;
+	private BigDecimal value;
+	private LocalDate date;
+
+	public Product(Long id, String description, BigDecimal value, LocalDate date) {
+		this.id = id;
+		this.description = description;
+		this.value = value;
+		this.date = date;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public BigDecimal getValue() {
+		return value;
+	}
+
+	public void setValue(BigDecimal value) {
+		this.value = value;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+}
